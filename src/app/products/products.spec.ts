60466aa66dd3a16933c221dd2727fae2d8cc8b7d
ErickCisneros <erickxz10@gmail.com>
2025-09-20 17:24:04 -0500

    test: products

--- src/app/products/products.spec.ts
+++ src/app/products/products.spec.ts
@@ -2,7 +2,27 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideLocationMocks } from '@angular/common/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
+import { ProductModel } from '../core/models/product.model';
 import Products from './products';
+
+const mockProducts: ProductModel[] = [
+  {
+    id: 'prd-001',
+    name: 'Laptop',
+    description: 'Powerful laptop',
+    logo: 'http://logo1.png',
+    date_release: '2025-01-01',
+    date_revision: '2026-01-01',
+  },
+  {
+    id: 'prd-002',
+    name: 'Phone',
+    description: 'Smartphone device',
+    logo: 'http://logo2.png',
+    date_release: '2025-02-01',
+    date_revision: '2026-02-01',
+  },
+];
 
 describe('Products', () => {
   let component: Products;
@@ -21,5 +41,15 @@ describe('Products', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should filter products by name', () => {
+    component['allProducts'].set(mockProducts);
+    component.products.set(mockProducts);
+    component.onSearch('laptop');
+
+    fixture.detectChanges();
+
+    expect(component.products()).toEqual([expect.objectContaining({ id: 'prd-001' })]);
   });
 });
